9e7e9cf56e0b931f917b2be6e86e4efa30b8dbab
nicolas <lopez>
2020-02-19 21:36:15 -0300

    modificaciones en vistas

--- public/js/navApp.js
+++ public/js/navApp.js
@@ -1,7 +1,36 @@
+/*
 function cambiarClase(){
     let siteNav = document.getElementById('site-nav');
         siteNav.classList.toggle('site-nav-open');
     let menuOpen = document.getElementById('menu-toggle');
         menuOpen.classList.toggle('menu-open');    
         
+}
+*/
+console.log("Js Running")
+function openNav() {
+  document.getElementById("myNav").style.width= "100%";
+  console.log("openNav function got fired");
+}
+
+function closeNav() {
+  console.log("closeNav function got fired");
+  document.getElementById("myNav").style.width= "0%";
+}
+
+/* (document.getElementByid("myNav").style.width="0"){
+  console.log("In if block");
+  document.getElementById("myNav").style.width="100%";
+  }*/
+
+function openNavFromTop() {
+  console.log("openNavFromTop() got fired");
+  //document.getElementById("myNav").style.width="100%";
+  
+  //document.getElementById("myNav").style.transition="0.0s";
+  
+  document.getElementById("myNav").style.height="100%";
+  
+  //document.getElementById("myNav").style.transition="1s";
+  
 }
--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,17 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-// ************ Function to Read an HTML File ************
-function readHTML (fileName) {
-	let filePath = path.join(__dirname, `/../views/${fileName}.html`);
-	let htmlFile = fs.readFileSync(filePath, 'utf-8');
-	return htmlFile;
-}
 
 const controller = {
 	root: (req, res) => {
-		let html = readHTML('index');
-		res.send(html);
+		res.render('index')
 	},
 };
 
