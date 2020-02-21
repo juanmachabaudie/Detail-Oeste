200255a67896e5b5f03461866927d51a37b39f21
juanmachabaudie <jumachabaudie@gmail.com>
2020-02-21 01:56:33 -0300

    bootstrap aplicado. faltan algunas correcciones

--- public/js/navApp.js
+++ public/js/navApp.js
@@ -1,36 +1,25 @@
-/*
-function cambiarClase(){
-    let siteNav = document.getElementById('site-nav');
-        siteNav.classList.toggle('site-nav-open');
-    let menuOpen = document.getElementById('menu-toggle');
-        menuOpen.classList.toggle('menu-open');    
-        
-}
-*/
-console.log("Js Running")
+/* Open when someone clicks on the span element */
 function openNav() {
-  document.getElementById("myNav").style.width= "100%";
-  console.log("openNav function got fired");
+  document.getElementById("myNav").style.width = "100%";
 }
 
+/* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-  console.log("closeNav function got fired");
-  document.getElementById("myNav").style.width= "0%";
+  document.getElementById("myNav").style.width = "0%";
 }
 
-/* (document.getElementByid("myNav").style.width="0"){
-  console.log("In if block");
-  document.getElementById("myNav").style.width="100%";
-  }*/
+var nav = document.getElementById('navbar');
 
-function openNavFromTop() {
-  console.log("openNavFromTop() got fired");
-  //document.getElementById("myNav").style.width="100%";
-  
-  //document.getElementById("myNav").style.transition="0.0s";
-  
-  document.getElementById("myNav").style.height="100%";
-  
-  //document.getElementById("myNav").style.transition="1s";
-  
+window.onscroll = function () {
+
+  if(window.pageYOffset > 100){
+
+    navbar.style.position = "fixed";
+    navbar.style.top = 0;
+
+  }else{
+    
+    navbar.style.position = "initial";
+    navbar.style.top = 100;
+  }  
 }
