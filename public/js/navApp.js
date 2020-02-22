7cbef9bf9d80ea49c2341bf519dc01b0d4c25539
juanmachabaudie <jumachabaudie@gmail.com>
2020-02-22 03:33:05 -0300

    Barra casi terminada

--- public/js/navApp.js
+++ public/js/navApp.js
@@ -1,3 +1,5 @@
+/*---------------Curtain Menu----------------*/
+
 /* Open when someone clicks on the span element */
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
@@ -6,20 +8,4 @@ function openNav() {
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
-}
-
-var nav = document.getElementById('navbar');
-
-window.onscroll = function () {
-
-  if(window.pageYOffset > 100){
-
-    navbar.style.position = "fixed";
-    navbar.style.top = 0;
-
-  }else{
-    
-    navbar.style.position = "initial";
-    navbar.style.top = 100;
-  }  
 }
